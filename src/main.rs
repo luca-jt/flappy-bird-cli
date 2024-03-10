10c334a7de04faf1465fe2137453752268c9cb24
luca-jt <random.lulu@die-genossen.de>
2024-03-10 15:35:03 +0100

    player render doesnt happen as much as it should

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-
 pub mod framerate_capper;
+use std::cmp::min;
 use std::io::{self, Write};
 use std::time::Duration;
 use crossterm::event::{self, KeyCode, KeyEvent, Event};
-
 pub use crate::framerate_capper::fps_capping::FpsCapper;
 
 
@@ -33,9 +32,13 @@ const BOARD_STR: &'static str = r#"
                                                                 
 "#;
 
-const FPS: u8 = 30;
+
+const FPS: u8 = 10;
+const PLAYER_JUMP_SPEED: u8 = 9;
+const G_FORCE: f32 = 9.81;
+const ANIMATION_SPEED: f32 = 1.0;
+
 const PLAYER_X: usize = 3;
-const PLAYER_JUMP_SPEED: u8 = 8;
 const LINE_LENGTH: usize = 64;
 const NUMBER_OF_LINES: usize = 24;
 const PLAYER_START_Y: usize = NUMBER_OF_LINES / 2;
@@ -58,17 +61,15 @@ fn space_pressed() -> bool
 }
 
 
-
-
-fn calc_player_pos(t: f32, v0: f32, h: i8) -> i8
+fn calc_player_pos(t: f32, v0: f32, h: i16) -> i16
 {
-    (0.5 * 9.81 * t*t - v0 * t) as i8 + h
+    (0.5 * G_FORCE * t*t - v0 * t) as i16 + h
 }
 
 
-fn set_player_pos(new_y: i8, former_y: i8, board: &mut Vec<Vec<char>>) -> bool
+fn set_player_pos(new_y: i16, former_y: i16, board: &mut Vec<Vec<char>>) -> bool
 {
-    if new_y > NUMBER_OF_LINES as i8 - 1 || new_y < 0 {
+    if new_y > NUMBER_OF_LINES as i16 - 1 || new_y < 0 {
         return false;
     }
     board[former_y as usize][PLAYER_X] = ' ';
@@ -106,6 +107,7 @@ fn print_board(board: &Vec<Vec<char>>, score: u16)
 {
     print!("\x1B[2J\x1B[H");
     io::stdout().flush().unwrap();
+
     println!("SCORE: {}", score);
     println!("----------------------------------------------------------------");
     for v in board {
@@ -130,7 +132,7 @@ fn main()
     let mut loop_counter: u16 = 0;
     let mut shift_speed: u8 = 1;
     let mut loops_since_keypress: u16 = 0;
-    let mut fps_capper: FpsCapper = FpsCapper::new(FPS);
+    let mut fps_capper = FpsCapper::new(FPS);
 
     let mut current_y = PLAYER_START_Y;
     board[current_y][PLAYER_X] = '@';
@@ -150,11 +152,15 @@ fn main()
 
         print_board(&board, score);
         
-        let new_y = calc_player_pos((loops_since_keypress / FPS as u16) as f32, PLAYER_JUMP_SPEED as f32, last_jump_y as i8);
-        running = set_player_pos(new_y, current_y as i8, &mut board); 
+        let new_y = calc_player_pos((loops_since_keypress / FPS as u16) as f32 * ANIMATION_SPEED,
+                                        PLAYER_JUMP_SPEED as f32,
+                                        last_jump_y as i16);
+
+        running = set_player_pos(new_y, current_y as i16, &mut board); 
         current_y = new_y as usize;
+        
         if check_if_col_passed(&board) {
-            score += 1
+            score += 1;
         }
         if space_pressed() {
             last_jump_y = current_y;
@@ -163,9 +169,9 @@ fn main()
 
         loop_counter %= (FPS / shift_speed) as u16;
         if loop_counter == 0 {
-            shift_cols(&mut board)
+            shift_cols(&mut board);
         }
-        if score % 10 == 0 && shift_speed < 10 {
+        if score % min(10, FPS as u16) == 0 && shift_speed < 10 && score != 0 {
             shift_speed += 1
         }
 
